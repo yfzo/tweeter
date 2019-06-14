e1b7f4cd21a0aa698caaf0146952484fc73c6b34
Yifei <yifeizhou.to@gmail.com>
2019-06-14 19:10:26 +0000

    Switched from in-memory db to MongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+//const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -9,19 +9,22 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection("tweets").insertOne(newTweet);
+      callback(null, true);
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
+      function getTweets(callback) {
+        db.collection("tweets").find().toArray(callback);
+      }
+
+      getTweets((err, tweets) => {
+        if (err) throw err;
+
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        callback(null, tweets.sort(sortNewestFirst));
       });
     }
-
   };
 }
